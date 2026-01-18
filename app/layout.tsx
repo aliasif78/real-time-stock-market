ac4dc7d7ea6d54a8d59395371e479c6d523e83dc
aliasif78 <aliasif1171@gmail.com>
2026-01-18 12:50:08 +0500

    feat: Implement authentication pages and integrate new form and UI components.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,26 @@
 // Next Js
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
 
 // Styles
-import "./globals.css";
+import './globals.css';
 
 // Fonts
-const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
-const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
+const geistSans = Geist({ variable: '--font-geist-sans', subsets: ['latin'] });
+const geistMono = Geist_Mono({ variable: '--font-geist-mono', subsets: ['latin'] });
 
 // Metadata
 export const metadata: Metadata = {
-  title: "Signalist",
-  description: "Track real-time stock prices, get personalized alerts and explore detailed company insights.",
+  title: 'Signalist',
+  description: 'Track real-time stock prices, get personalized alerts and explore detailed company insights.',
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className="dark">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
